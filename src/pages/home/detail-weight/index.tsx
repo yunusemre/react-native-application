33201e37f8e8f365da79f7ed86e25eb335bfadd4
Yunus Emre Tatar <yunusemre.tatar@kolaygelsin.com>
2023-06-14 23:54:37 +0300

    added detail weight page

--- src/pages/home/detail-weight/index.tsx
+++ src/pages/home/detail-weight/index.tsx
@@ -20,10 +20,10 @@ export const HomeDetailWeights = ({ route }: any) => {
           bg="white"
           p={8}
           m={8}
-          height={180}
+          height={195}
         >
           <Box flexDirection="row" p={3} justifyContent="space-between">
-            <Text style={[globalStyle.w50, { fontSize: 10 }]}>Barcode Kodu:</Text>
+            <Text style={[globalStyle.w50, { fontSize: 10 }]}>Barcode No:</Text>
             <Text style={[globalStyle.w50, { fontSize: 10 }]}>
               {itemListDetail.CustomerBarcode}
             </Text>
@@ -38,47 +38,55 @@ export const HomeDetailWeights = ({ route }: any) => {
           </Box>
           <Box flexDirection="row">
             <Box width={'50%'}>
-              <Box flexDirection="row" p={1}>
+              <Box flexDirection="row" p={2}>
                 <Text style={[globalStyle.w50, { fontSize: 10 }]}>En:</Text>
-                <Text style={[globalStyle.w50, globalStyle.bold, { fontSize: 10 }]}>12</Text>
+                <Text style={[globalStyle.w50, globalStyle.bold, { fontSize: 10 }]}>
+                  {itemListDetail.Width}
+                </Text>
               </Box>
             </Box>
             <Box width={'50%'} flexDirection="row">
-              <Box flexDirection="row" p={1}>
+              <Box flexDirection="row" p={2}>
                 <Text style={[globalStyle.w50, { fontSize: 10 }]}>En:</Text>
-                <Text style={[globalStyle.w50, globalStyle.bold, { fontSize: 10 }]}>12</Text>
+                <Text style={[globalStyle.w50, globalStyle.bold, { fontSize: 10 }]}>0.0</Text>
               </Box>
             </Box>
           </Box>
           <Box flexDirection="row">
-            <Box width={'50%'} flexDirection="row" p={1}>
+            <Box width={'50%'} flexDirection="row" p={2}>
               <Text style={[globalStyle.w50, { fontSize: 10 }]}>Boy:</Text>
-              <Text style={[globalStyle.w50, globalStyle.bold, { fontSize: 10 }]}>12</Text>
+              <Text style={[globalStyle.w50, globalStyle.bold, { fontSize: 10 }]}>
+                {itemListDetail.Length}
+              </Text>
             </Box>
-            <Box width={'50%'} flexDirection="row" p={1}>
+            <Box width={'50%'} flexDirection="row" p={2}>
               <Text style={[globalStyle.w50, { fontSize: 10 }]}>Boy:</Text>
-              <Text style={[globalStyle.w50, globalStyle.bold, { fontSize: 10 }]}>12</Text>
+              <Text style={[globalStyle.w50, globalStyle.bold, { fontSize: 10 }]}>0.0</Text>
             </Box>
           </Box>
           <Box flexDirection="row">
-            <Box width={'50%'} flexDirection="row" p={1}>
+            <Box width={'50%'} flexDirection="row" p={2}>
               <Text style={[globalStyle.w50, { fontSize: 10 }]}>Yükseklik:</Text>
-              <Text style={[globalStyle.w50, globalStyle.bold, { fontSize: 10 }]}>12</Text>
+              <Text style={[globalStyle.w50, globalStyle.bold, { fontSize: 10 }]}>
+                {itemListDetail.Height}
+              </Text>
             </Box>
             <Box width={'50%'} flexDirection="row">
-              <Box flexDirection="row" p={1}>
+              <Box flexDirection="row" p={2}>
                 <Text style={[globalStyle.w50, { fontSize: 10 }]}>Yükseklik:</Text>
                 <Text style={[globalStyle.w50, globalStyle.bold, { fontSize: 10 }]}>12</Text>
               </Box>
             </Box>
           </Box>
           <Box flexDirection="row">
-            <Box width={'50%'} flexDirection="row" p={1}>
+            <Box width={'50%'} flexDirection="row" p={2}>
               <Text style={[globalStyle.w50, { fontSize: 10 }]}>Desi:</Text>
-              <Text style={[globalStyle.w50, globalStyle.bold, { fontSize: 10 }]}>12</Text>
+              <Text style={[globalStyle.w50, globalStyle.bold, { fontSize: 10 }]}>
+                {itemListDetail.Deci.toFixed(1)}
+              </Text>
             </Box>
             <Box width={'50%'} flexDirection="row">
-              <Box flexDirection="row" p={1}>
+              <Box flexDirection="row" p={2}>
                 <Text style={[globalStyle.w50, { fontSize: 10 }]}>Desi:</Text>
                 <Text style={[globalStyle.w50, globalStyle.bold, { fontSize: 10 }]}>12</Text>
               </Box>
@@ -86,13 +94,15 @@ export const HomeDetailWeights = ({ route }: any) => {
           </Box>
           <Box flexDirection="row">
             <Box width={'50%'}>
-              <Box flexDirection="row">
+              <Box flexDirection="row" p={2}>
                 <Text style={[globalStyle.w50, { fontSize: 10 }]}>Kilo:</Text>
-                <Text style={[globalStyle.w50, globalStyle.bold, { fontSize: 10 }]}>12</Text>
+                <Text style={[globalStyle.w50, globalStyle.bold, { fontSize: 10 }]}>
+                  {itemListDetail.Weight}
+                </Text>
               </Box>
             </Box>
             <Box width={'50%'} flexDirection="row">
-              <Box flexDirection="row">
+              <Box flexDirection="row" p={2}>
                 <Text style={[globalStyle.w50, { fontSize: 10 }]}>Kilo:</Text>
                 <Text style={[globalStyle.w50, globalStyle.bold, { fontSize: 10 }]}>12</Text>
               </Box>
@@ -100,18 +110,18 @@ export const HomeDetailWeights = ({ route }: any) => {
           </Box>
           <Box flexDirection="row">
             <Box width={'50%'} bg="primary">
-              <Box flexDirection="row" p={1}>
+              <Box flexDirection="row" p={2}>
                 <Text style={[globalStyle.w50, { fontSize: 10, color: 'white' }]}>Tutar:</Text>
                 <Text style={[globalStyle.w50, globalStyle.bold, { fontSize: 10, color: 'white' }]}>
-                  12.2
+                  {itemListDetail.UnitPrice}
                 </Text>
               </Box>
             </Box>
             <Box width={'50%'} flexDirection="row" bg="color">
-              <Box flexDirection="row" p={1}>
+              <Box flexDirection="row" p={2}>
                 <Text style={[globalStyle.w50, { fontSize: 10, color: 'white' }]}>Tutar:</Text>
                 <Text style={[globalStyle.w50, globalStyle.bold, { fontSize: 10, color: 'white' }]}>
-                  12.2
+                  0.0
                 </Text>
               </Box>
             </Box>
